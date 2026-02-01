18e6f450aba453c24932487aced62314cfd7c4f0
Nirav Joshi <niravjoshi3000@gmail.com>
2026-02-01 21:18:34 +0800

    implemented stake, unstake and claim

--- anchor-nft-staking-starter/programs/anchor-nft-staking-q4-25/src/instructions/stake.rs
+++ anchor-nft-staking-starter/programs/anchor-nft-staking-q4-25/src/instructions/stake.rs
@@ -10,13 +10,79 @@ use crate::{
     state::{StakeAccount, StakeConfig, UserAccount},
 };
 
-// #[derive(Accounts)]
-// pub struct Stake<'info> {
-//TODO
-// }
+#[derive(Accounts)]
+pub struct Stake<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
 
-// impl<'info> Stake<'info> {
-//     pub fn stake(&mut self, bumps: &StakeBumps) -> Result<()> {
-//TODO
-//     }
-// }
+    /// CHECK: Validated by Core program CPI
+    #[account(mut)]
+    pub asset: UncheckedAccount<'info>,
+
+    /// CHECK: Validated by Core program CPI
+    #[account(mut)]
+    pub collection: UncheckedAccount<'info>,
+
+    #[account(
+        init,
+        payer = user,
+        seeds = [b"stake", config.key().as_ref(), asset.key().as_ref()],
+        bump,
+        space = StakeAccount::DISCRIMINATOR.len() + StakeAccount::INIT_SPACE,
+    )]
+    pub stake_account: Account<'info, StakeAccount>,
+
+    #[account(
+        seeds = [b"config".as_ref()],
+        bump = config.bump,
+    )]
+    pub config: Account<'info, StakeConfig>,
+
+    #[account(
+        mut,
+        seeds = [b"user".as_ref(), user.key().as_ref()],
+        bump = user_account.bump,
+    )]
+    pub user_account: Account<'info, UserAccount>,
+
+    #[account(address = CORE_PROGRAM_ID)]
+    /// CHECK: Metaplex Core program
+    pub core_program: UncheckedAccount<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Stake<'info> {
+    pub fn stake(&mut self, bumps: &StakeBumps) -> Result<()> {
+        require!(
+            self.user_account.amount_staked < self.config.max_stake,
+            StakeError::MaxStakeReached
+        );
+
+        // Add FreezeDelegate plugin with stake_account as authority
+        AddPluginV1CpiBuilder::new(&self.core_program.to_account_info())
+            .asset(&self.asset.to_account_info())
+            .collection(Some(&self.collection.to_account_info()))
+            .payer(&self.user.to_account_info())
+            .authority(None)
+            .system_program(&self.system_program.to_account_info())
+            .plugin(Plugin::FreezeDelegate(FreezeDelegate { frozen: true }))
+            .init_authority(PluginAuthority::Address {
+                address: self.stake_account.key(),
+            })
+            .invoke()?;
+
+        // Initialize stake account
+        self.stake_account.set_inner(StakeAccount {
+            owner: self.user.key(),
+            mint: self.asset.key(),
+            staked_at: Clock::get()?.unix_timestamp,
+            bump: bumps.stake_account,
+        });
+
+        // Increment staked count
+        self.user_account.amount_staked += 1;
+
+        Ok(())
+    }
+}
--- anchor-nft-staking-starter/programs/anchor-nft-staking-q4-25/src/instructions/unstake.rs
+++ anchor-nft-staking-starter/programs/anchor-nft-staking-q4-25/src/instructions/unstake.rs
@@ -10,13 +10,92 @@ use crate::{
     state::{StakeAccount, StakeConfig, UserAccount},
 };
 
-// #[derive(Accounts)]
-// pub struct Unstake<'info> {
-// //TODO
-// }
+#[derive(Accounts)]
+pub struct Unstake<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
 
-// impl<'info> Unstake<'info> {
-//     pub fn unstake(&mut self) -> Result<()> {
-// //TODO
-//     }
-// }
+    /// CHECK: Validated by Core program CPI
+    #[account(mut)]
+    pub asset: UncheckedAccount<'info>,
+
+    /// CHECK: Validated by Core program CPI
+    #[account(mut)]
+    pub collection: UncheckedAccount<'info>,
+
+    #[account(
+        mut,
+        close = user,
+        seeds = [b"stake", config.key().as_ref(), asset.key().as_ref()],
+        bump = stake_account.bump,
+        constraint = stake_account.owner == user.key() @ StakeError::NotOwner,
+    )]
+    pub stake_account: Account<'info, StakeAccount>,
+
+    #[account(
+        seeds = [b"config".as_ref()],
+        bump = config.bump,
+    )]
+    pub config: Account<'info, StakeConfig>,
+
+    #[account(
+        mut,
+        seeds = [b"user".as_ref(), user.key().as_ref()],
+        bump = user_account.bump,
+    )]
+    pub user_account: Account<'info, UserAccount>,
+
+    #[account(address = CORE_PROGRAM_ID)]
+    /// CHECK: Metaplex Core program
+    pub core_program: UncheckedAccount<'info>,
+
+    pub system_program: Program<'info, System>,
+}
+
+impl<'info> Unstake<'info> {
+    pub fn unstake(&mut self) -> Result<()> {
+        let time_elapsed = ((Clock::get()?.unix_timestamp - self.stake_account.staked_at)
+            / 86400) as u32;
+
+        require!(
+            time_elapsed >= self.config.freeze_period,
+            StakeError::FreezePeriodNotPassed
+        );
+
+        // Calculate and award points
+        self.user_account.points += time_elapsed * self.config.points_per_stake as u32;
+
+        // Signer seeds for stake_account PDA
+        let signer_seeds: &[&[&[u8]]] = &[&[
+            b"stake",
+            self.config.to_account_info().key.as_ref(),
+            self.asset.to_account_info().key.as_ref(),
+            &[self.stake_account.bump],
+        ]];
+
+        // Unfreeze the NFT (requires PDA signature)
+        UpdatePluginV1CpiBuilder::new(&self.core_program.to_account_info())
+            .asset(&self.asset.to_account_info())
+            .collection(Some(&self.collection.to_account_info()))
+            .payer(&self.user.to_account_info())
+            .authority(Some(&self.stake_account.to_account_info()))
+            .system_program(&self.system_program.to_account_info())
+            .plugin(Plugin::FreezeDelegate(FreezeDelegate { frozen: false }))
+            .invoke_signed(signer_seeds)?;
+
+        // Remove the FreezeDelegate plugin (user is authority after unfreeze)
+        RemovePluginV1CpiBuilder::new(&self.core_program.to_account_info())
+            .asset(&self.asset.to_account_info())
+            .collection(Some(&self.collection.to_account_info()))
+            .payer(&self.user.to_account_info())
+            .authority(None)
+            .system_program(&self.system_program.to_account_info())
+            .plugin_type(PluginType::FreezeDelegate)
+            .invoke()?;
+
+        // Decrement staked count
+        self.user_account.amount_staked -= 1;
+
+        Ok(())
+    }
+}
